f19428a1dd973005c141f5a938ce5a844e09a793
Nakazaki4 <nakazakinakazaki22@gmail.com>
2026-05-10 19:20:57 +0100

    Initial setup: Spring Boot + Angular skeleton

---
+++ backend/src/test/java/com/zone01/_blog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zone01._blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
