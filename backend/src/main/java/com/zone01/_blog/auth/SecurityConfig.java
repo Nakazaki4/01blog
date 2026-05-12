132028e44696a1d81cb2e015a6f452f18890ce14
Nakazaki4 <nakazakinakazaki22@gmail.com>
2026-05-12 18:54:53 +0100

    security done

--- backend/src/main/java/com/zone01/_blog/auth/SecurityConfig.java
+++ backend/src/main/java/com/zone01/_blog/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zone01._blog.auth;
 import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
